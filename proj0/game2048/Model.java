a47eb4d74efb97a6948741fa74da2e9c1582b68f
Frank <frankhou@utexas.edu>
2023-11-18 14:52:02 -0600

    Updated 2048

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -114,7 +114,7 @@ public class Model extends Observable {
         // for the tilt to the Side SIDE. If the board changed, set the
         // changed local variable to true.
 
-
+        board.setViewingPerspective(side);
         for (int col = 0; col < board.size(); col += 1) {
             for (int row = board.size() - 1; row >= 0; row -= 1) {
                 Tile t1 = board.tile(col, row);
